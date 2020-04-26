aab9d4e94580544c165ba779ba2801bab198c038
Tiago <t.marques999@gmail.com>
2020-04-26 13:26:00 +0100

    Add paths for home and movie screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { Home } from './screens';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, Movie } from './screens';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/movie/:movieId'>
+          <Movie />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
