2fb31cd31df5272d39c39911044875b4a887c898
Tiago <t.marques999@gmail.com>
2020-05-03 17:55:40 +0100

    Rename movie routes file and remove unused dependency

--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const bodyParser = require('body-parser');
 
 router.get('/movie/:movieId', async (req, res) => {
   const { movieId } = req.params;
