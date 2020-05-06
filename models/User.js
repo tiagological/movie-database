3ed0dcbd494b889f1825e5b5acf709d037643142
Tiago <t.marques999@gmail.com>
2020-05-06 19:11:58 +0100

    Create new user schema

--- models/User.js
+++ models/User.js
@@ -1,23 +1,48 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import { compareSync, hashSync } from 'bcryptjs';
+
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (username) => User.doesNotExist({ username }),
+        message: 'Username already exists',
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (email) => User.doesNotExist({ email }),
+        message: 'Email already exists',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now(),
-  },
+  { timestamps: true }
+);
+
+UserSchema.pre('save', function () {
+  if (this.isModified('password')) {
+    this.password = hashSync(this.password, 10);
+  }
 });
 
-module.exports = User = mongoose.model('users', UserSchema);
+UserSchema.statics.doesNotExist = async function (field) {
+  return (await this.where(field).countDocuments()) === 0;
+};
+
+UserSchema.methods.comparePasswords = function (password) {
+  return compareSync(password, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
