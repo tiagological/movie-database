58603b0c46a5beb7f980396a3d62a550e02a3f4b
Tiago <t.marques999@gmail.com>
2020-05-06 19:24:37 +0100

    Create global styles for frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home, Movie } from './screens';
 
@@ -16,5 +17,28 @@ function App() {
     </Router>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    height: 100vh;
+    width: 100%;
+    
+    box-sizing: border-box;
+	  font-family: 'Montserrat', sans-serif;
+    background: #fafafa;
+  }
+  *, *:before, *:after {
+  box-sizing: inherit;
+    }
+  #root {
+    height: 100%;
+    width: 100%;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  `;
 
 export default App;
