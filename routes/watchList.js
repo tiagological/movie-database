4dfd0532f362f1682f8ecaa32809bdfedb688f8a
Tiago <t.marques999@gmail.com>
2020-05-10 20:45:42 +0100

    Create route to fetch user watchlist from database

--- routes/watchList.js
+++ routes/watchList.js
@@ -5,7 +5,39 @@ import { parseError } from '../util/helpers';
 
 const router = express.Router();
 
-router.post('', async (req, res) => {
+router.get('/fetch', async (req, res) => {
+  try {
+    const { session } = req;
+
+    const sessionUser = session.user;
+
+    if (sessionUser) {
+      const { userId } = sessionUser;
+
+      const doc = await User.findById(userId);
+
+      const watchList = doc.watch_list.toObject();
+
+      let filteredWatchList = [];
+
+      if (watchList.length > 0) {
+        filteredWatchList = watchList.map((movie) => {
+          delete movie._id;
+          return movie;
+        });
+      }
+
+      res.send(filteredWatchList);
+    } else {
+      throw new Error('You need to be logged in order to load your watch list');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: parseError(err) });
+  }
+});
+
+router.post('/add', async (req, res) => {
   try {
     const { id, title, poster_path, runtime, release_date } = req.body;
 
