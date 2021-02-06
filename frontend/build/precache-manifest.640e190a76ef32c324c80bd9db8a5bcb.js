5800fac36a469fde41a50668cc06af1663125dee
Tiago <t.marques999@gmail.com>
2021-02-06 13:05:22 +0000

    Unignore build folder

---
+++ frontend/build/precache-manifest.640e190a76ef32c324c80bd9db8a5bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e7d6cbb9bb9a22edf90757a5bb2483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e8bd58de246c1db8eb",
+    "url": "/static/js/2.2fb7ec8b.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/static/js/2.2fb7ec8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aed185d8d307e2eff2f",
+    "url": "/static/js/main.3e082825.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "cb61ac4fca1dd4fbbcb5a2e7b3403d5c",
+    "url": "/static/media/empty-list-svg.cb61ac4f.svg"
+  },
+  {
+    "revision": "2a3111e5ab7aadbd2d56f57cbf56d1cc",
+    "url": "/static/media/logo.2a3111e5.svg"
+  }
+]);
